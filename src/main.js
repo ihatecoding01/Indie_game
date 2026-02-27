8036ebd9ea416868a99050ff1037e53e2d172ada
Percival Trinidade <percivaltrinidade6092@gmail.com>
2026-02-27 17:32:56 +0530

    added comments in main.js

--- src/main.js
+++ src/main.js
@@ -1,34 +1,33 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; 
+import Stats from 'three/addons/libs/stats.module.js' // adds a FPS counter and other features
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'; //lets you control without a mouse
+// imported Terrain function from terrain.js
 import { Terrain } from './terrain';
 
 const gui = new GUI();
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene(); //the base
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); //sets up the camera
 
-
-
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer(); // three.js runs on webgl
 const controls = new OrbitControls( camera, renderer.domElement );
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const terrain = new Terrain(10, 10);
+const terrain = new Terrain(10, 10); //sets up a terrain of 10 by 10 from the function we imported earlier
 scene.add(terrain);
 
-const lamp = new THREE.DirectionalLight();
+const lamp = new THREE.DirectionalLight(); // the lighting setup 
 lamp.position.set(1, 2, 3);
 scene.add(lamp);
 
-const ambient = new THREE.AmbientLight();
-ambient.intensity = 0.5;
+const ambient = new THREE.AmbientLight(); // basic lighting for the whole workspace
+ambient.intensity = 0.5; 
 scene.add(ambient);
 
-const stats = new Stats();
+const stats = new Stats(); //adds the stats functionality we imported earlier
 document.body.appendChild(stats.dom)
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
@@ -38,13 +37,15 @@ scene.add( cube );
 camera.position.z = 5;
 controls.update();
 
-function animate( time ) {
+// the main function that does the rendering of the cube
+function animate( time ) { 
     controls.update();
     stats.update();
     renderer.render( scene, camera );
 
 }
 
+// for better panning and control
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
--- src/terrain.js
+++ src/terrain.js
@@ -8,7 +8,7 @@ export class Terrain extends THREE.Mesh {
         this.height = height;
 
         this.geometry = new THREE.PlaneGeometry(width, height);
-        this.material = new THREE.MeshStandardMaterial({color : 0x50a00});
+        this.material = new THREE.MeshStandardMaterial({color : 0x50a000});
 
         this.rotation.x = -Math.PI / 2;
     }
